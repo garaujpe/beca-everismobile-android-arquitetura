861cb810cc08064579541ca1bca88b151fac48b2
Guilherme Araujo <guilherme.araujo.pereira@everis.com>
2021-04-05 17:05:05 -0300

    Ajuste na Class 'Repository' e criação de constantes para eliminar strings soltas no codigo

--- app/src/main/java/com/everis/becakotlinmvvm/Model/HolidayRepository.kt
+++ app/src/main/java/com/everis/becakotlinmvvm/Model/HolidayRepository.kt
@@ -2,7 +2,6 @@ package com.everis.becakotlinmvvm.Model
 
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
-import com.everis.becakotlinmvvm.Api.ApiInterface
 import com.everis.becakotlinmvvm.Api.RetrofitClient
 import com.everis.becakotlinmvvm.HolidayModel
 import retrofit2.Call
@@ -11,36 +10,40 @@ import retrofit2.Response
 
 class HolidayRepository {
 
+    companion object{
+        const val RESPONSE = "onResponse response="
+        const val RESPONSE_SIZE = "onResponse response.size="
+        const val FAILURE = "onFailure call="
+    }
+
     val TAG: String = javaClass.simpleName
 
     fun fetchHolidays(): MutableLiveData<List<HolidayModel>> {
-        val mutableList: MutableLiveData<List<HolidayModel>> = MutableLiveData()
+        val holidayMutableList: MutableLiveData<List<HolidayModel>> = MutableLiveData()
 
-        val apiInterface = RetrofitClient.getRetrofitInstance("https://date.nager.at/api/v2/")
-            .create(ApiInterface::class.java)
+        val apiInterface = RetrofitClient.retrofitApi()
 
         apiInterface.getHolidays().enqueue(object : Callback<List<HolidayModel>> {
             override fun onResponse(
                 call: Call<List<HolidayModel>>,
-                response: Response<List<HolidayModel>>
+                responseHoliday: Response<List<HolidayModel>>
             ) {
-                Log.e(TAG, "onResponse response=" + response.toString())
+                Log.e(TAG, RESPONSE + responseHoliday.toString())
 
-                if (response.isSuccessful) {
-                    Log.e(TAG, "onResponse response.size=" + response.body()?.size)
+                if (responseHoliday.isSuccessful) {
+                    Log.e(TAG, RESPONSE_SIZE + responseHoliday.body()?.size)
 
-                    if (response.body() != null && response.body()?.size!! > 0) {
-                        mutableList.value = response.body()!!
+                    if (responseHoliday.body() != null && responseHoliday.body()?.size!! > 0) {
+                        holidayMutableList.value = responseHoliday.body()!!
                     }
                 }
             }
 
             override fun onFailure(call: Call<List<HolidayModel>>, t: Throwable) {
-                Log.e(TAG, "onFailure call=" + call.toString())
+                Log.e(TAG, FAILURE + call.toString())
             }
-
         })
 
-        return mutableList
+        return holidayMutableList
     }
 }
